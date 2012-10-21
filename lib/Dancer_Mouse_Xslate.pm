4a241476f4979b27201b4b4e662fd682d0eaa920
ditto572 <ditto572@gmail.com>
2012-10-21 23:16:21 +0900

    Dancer install

--- lib/Dancer_Mouse_Xslate.pm
+++ lib/Dancer_Mouse_Xslate.pm
@@ -4,7 +4,25 @@ use Dancer ':syntax';
 our $VERSION = '0.1';
 
 get '/' => sub {
-    template 'index';
+	    template 'index';
+};
+
+get '/hello/:name' => sub {
+    return "Why, hello there " . param('name');
+};
+
+get '/bugs/' => sub {
+	    my $xml = XML::Simple->new;
+		    my $data = $xml->XMLin($filename);
+
+			    my $count = 0;
+				    my $list = "";
+					    foreach my $case (@{$data->{cases}->{case}}) {
+							        $list .= "<li><a href='http://bugs.movabletype.org/default.asp?$case->{ixBug}'>$case->{ixBug}</a> $case->{dtOpened}  : " . encode_entities($case->{sTitle}) . "</li>";
+									        $count++;
+											    }
+						    template 'bugs' => {title => 'Cases in Iliad', filename => $filename, count => $count, list => $list};
+							    
 };
 
 true;
