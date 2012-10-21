8dcdf3917dd54accd2671963459cb5e5fb78095b
ditto572 <ditto572@gmail.com>
2012-10-22 00:11:16 +0900

    two

--- lib/Dancer_Mouse_Xslate.pm
+++ lib/Dancer_Mouse_Xslate.pm
@@ -11,18 +11,18 @@ get '/hello/:name' => sub {
     return "Why, hello there " . param('name');
 };
 
-get '/bugs/' => sub {
-	    my $xml = XML::Simple->new;
-		    my $data = $xml->XMLin($filename);
-
-			    my $count = 0;
-				    my $list = "";
-					    foreach my $case (@{$data->{cases}->{case}}) {
-							        $list .= "<li><a href='http://bugs.movabletype.org/default.asp?$case->{ixBug}'>$case->{ixBug}</a> $case->{dtOpened}  : " . encode_entities($case->{sTitle}) . "</li>";
-									        $count++;
-											    }
-						    template 'bugs' => {title => 'Cases in Iliad', filename => $filename, count => $count, list => $list};
-							    
-};
+#get '/bugs/' => sub {
+#    my $xml = XML::Simple->new;
+#    my $data = $xml->XMLin($filename);
+#
+#    my $count = 0;
+#    my $list = "";
+#    foreach my $case (@{$data->{cases}->{case}}) {
+#        $list .= "<li><a href='http://bugs.movabletype.org/default.asp?$case->{ixBug}'>$case->{ixBug}</a> $case->{dtOpened}  : " . encode_entities($case->{sTitle}) . "</li>";
+#        $count++;
+#    }
+#    template 'bugs' => {title => 'Cases in Iliad', filename => $filename, count => $count, list => $list};
+#							    
+#};
 
 true;
